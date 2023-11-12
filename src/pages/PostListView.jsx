af0c2050095799273350260e5657a6096e38378c
GyunHee <ef3232@naver.com>
2023-11-12 22:34:32 +0900

    Fix: post CSS -> styled-component PostListView

--- src/pages/PostListView.jsx
+++ src/pages/PostListView.jsx
@@ -1,50 +1,218 @@
 import React, { useState } from 'react';
-import styles from './PostListView.module.css';
+import styled from 'styled-components';
 
-export default function PostListView() {
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const BannerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  background: url('/mock/hotBanner.png');
+  background-size: cover;
+  align-items: center;
+`;
+
+const HotBanner = styled.p`
+  padding: 8px;
+  border-radius: 10px;
+  gap: 4px;
+  background-color: #ffffff;
+  font-family: Pretendard;
+  font-size: 12px;
+  font-weight: 600;
+  color: #ff0000;
+  margin-right: 0.3rem;
+`;
+
+const TitleBanner = styled.p`
+  font-family: Pretendard;
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 28px;
+  letter-spacing: -0.5px;
+  text-align: left;
+  color: #ffffff;
+  margin-left: 0.3rem;
+`;
+
+const FilterBox = styled.div`
+  margin: 1rem;
+`;
+
+const UncheckedButton = styled.button`
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: -0.5px;
+  text-align: left;
+  padding: 0.5rem 0.8rem;
+  &.checked {
+    color: #0085ff;
+    border-bottom: 2px solid #0085ff;
+  }
+`;
+
+const TimeSortBox = styled.div`
+  margin: 0 0.7rem;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const UnRecommendedButton = styled.button`
+  font-family: Pretendard;
+  font-size: 12px;
+  font-weight: 600;
+  letter-spacing: -0.5px;
+  text-align: center;
+  padding: 0.6rem 0.7rem;
+  background-color: #eeeeee;
+  margin: 0.3rem;
+  border-radius: 0.5rem;
+  &.recommended {
+    background: #0085ff;
+    color: white;
+  }
+`;
+
+const SelectContainer = styled.div`
+  select {
+    font-family: Pretendard;
+    font-size: 16px;
+    font-weight: 600;
+    letter-spacing: -0.5px;
+    text-align: left;
+    padding: 0.5rem 0.8rem;
+  }
+`;
+
+const PostContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 1rem;
+`;
+
+const PostTitle = styled.p`
+  font-family: Pretendard;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: -0.5px;
+  text-align: left;
+  margin: 0;
+`;
+
+const PostInfo = styled.p`
+  font-family: Pretendard;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 14px;
+  letter-spacing: -0.5px;
+  text-align: left;
+  margin: 0;
+  color: #bdbdbd;
+`;
+
+const PostImg = styled.div`
+  background: url('/mock/postImg.png');
+  background-size: cover;
+  width: 60px;
+  height: 60px;
+`;
+
+const PostInfoContainer = styled.div`
+  display: flex;
+`;
+
+const LikeContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PostLike = styled.div`
+  background: url('/icons/like.png');
+  background-size: cover;
+  width: 24px;
+  height: 24px;
+  margin-right: 0.2rem;
+`;
+
+const LikeCnt = styled.p`
+  font-family: Pretendard;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 14px;
+  letter-spacing: -0.5px;
+  text-align: left;
+  padding-right: 1rem;
+  border-right: 2px solid #e0e0e0;
+`;
+
+const CommentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 1.3rem;
+`;
+
+const CommentIcon = styled.div`
+  background: url('/icons/comment.png');
+  background-size: cover;
+  width: 21px;
+  height: 19.38px;
+  margin-right: 0.2rem;
+`;
+
+const CommentCnt = styled.p`
+  font-family: Pretendard;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 14px;
+  letter-spacing: -0.5px;
+  text-align: left;
+`;
+
+const PostListView = () => {
   const [isHot, setIsHot] = useState(false);
   const [isRecommend, setIsRecommend] = useState(true);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.banner__container}>
-        <p className={styles.banner__hot}>🔥 HOT</p>
-        <p className={styles.banner__title}>광화문 우크라이나 시위</p>
-      </div>
-      <div className={styles.filter__box}>
-        <button
-          className={`${styles.unChecked} ${!isHot ? styles.checked : ''}`}
+    <Container>
+      <BannerContainer>
+        <HotBanner>🔥 HOT</HotBanner>
+        <TitleBanner>광화문 우크라이나 시위</TitleBanner>
+      </BannerContainer>
+      <FilterBox>
+        <UncheckedButton
+          className={!isHot ? 'checked' : ''}
           onClick={() => setIsHot(false)}
         >
           전체
-        </button>
-        <button
-          className={`${styles.unChecked} ${isHot ? styles.checked : ''}`}
+        </UncheckedButton>
+        <UncheckedButton
+          className={isHot ? 'checked' : ''}
           onClick={() => setIsHot(true)}
         >
           HOT
-        </button>
-      </div>
-      <div className={styles.timeSort__box}>
+        </UncheckedButton>
+      </FilterBox>
+      <TimeSortBox>
         <div>
-          <button
-            className={`${styles.unRecommended} ${
-              isRecommend ? styles.recommended : ''
-            }`}
+          <UnRecommendedButton
+            className={isRecommend ? 'recommended' : ''}
             onClick={() => setIsRecommend(true)}
           >
             추천순
-          </button>
-          <button
-            className={`${styles.unRecommended} ${
-              !isRecommend ? styles.recommended : ''
-            }`}
+          </UnRecommendedButton>
+          <UnRecommendedButton
+            className={!isRecommend ? 'recommended' : ''}
             onClick={() => setIsRecommend(false)}
           >
             최신순
-          </button>
+          </UnRecommendedButton>
         </div>
-        <div className="selectContainer">
+        <SelectContainer>
           <select name="" id="">
             <option value="전체" disabled selected>
               전체
@@ -53,25 +221,27 @@ export default function PostListView() {
             <option value="시위">시위</option>
             <option value="축제">축제</option>
           </select>
-        </div>
-      </div>
-      <div className={styles.post__container}>
+        </SelectContainer>
+      </TimeSortBox>
+      <PostContainer>
         <div>
-          <p className={styles.post__title}>광화문 우크라이나 시위</p>
-          <p className={styles.postInfo}>3시간전 조회수 732910회</p>
-          <div className={styles.postInfo__container}>
-            <div className={styles.like__container}>
-              <div className={styles.postLike}></div>
-              <p className={styles.likeCnt}>7,221개</p>
-            </div>
-            <div className={styles.comment__container}>
-              <div className={styles.commentIcon}></div>
-              <p className={styles.commentCnt}>4,413개</p>
-            </div>
-          </div>
+          <PostTitle>광화문 우크라이나 시위</PostTitle>
+          <PostInfo>3시간전 조회수 732910회</PostInfo>
+          <PostInfoContainer>
+            <LikeContainer>
+              <PostLike></PostLike>
+              <LikeCnt>7,221개</LikeCnt>
+            </LikeContainer>
+            <CommentContainer>
+              <CommentIcon></CommentIcon>
+              <CommentCnt>4,413개</CommentCnt>
+            </CommentContainer>
+          </PostInfoContainer>
         </div>
-        <div className={styles.postImg}></div>
-      </div>
-    </div>
+        <PostImg></PostImg>
+      </PostContainer>
+    </Container>
   );
-}
+};
+
+export default PostListView;

31f802a66b234653109e9e8553c20352d06a88c7
ijaesin <ijaesinii@gmail.com>
2023-11-30 16:55:11 +0900

    카카오 맵 api 추가

--- src/pages/map/MapView.jsx
+++ src/pages/map/MapView.jsx
@@ -1,7 +1,23 @@
+import { useEffect } from 'react';
+import styled from 'styled-components';
+
+const { kakao } = window;
+
+const MapContainer = styled.div`
+  width: 500px;
+  height: 400px;
+`;
+
 export default function MabView() {
-  return (
-    <div>
-      <div>map view</div>
-    </div>
-  );
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    const map = new kakao.maps.Map(container, options);
+  }, []);
+
+  return <MapContainer id="map" />;
 }
